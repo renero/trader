3c25971bd2f4e49f360e3715ccb3af904d78496e
Jesus Renero <jesus.renero@gmail.com>
2019-05-19 13:14:09 +0200

    simplified NN code.

--- trader.py
+++ trader.py
@@ -11,54 +11,7 @@ from keras.models import Sequential
 from myenv import *
 
 
-def q_learning_with_table(env, num_episodes=500):
-    q_table = np.zeros((env.num_states_, env.num_actions_))
-    y = 0.95
-    lr = 0.8
-    for i in range(num_episodes):
-        s = env.reset()
-        done = False
-        while not done:
-            if np.sum(q_table[s, :]) == 0:
-                # make a random selection of actions
-                a = np.random.randint(0, env.num_actions_)
-            else:
-                # select the action with largest q value in state s
-                a = np.argmax(q_table[s, :])
-            new_s, r, done, _ = env.step(a)
-            q_table[s, a] += r + lr * (
-                    y * np.max(q_table[new_s, :]) - q_table[s, a])
-            s = new_s
-        print('\n--')
-    return q_table
-
-
-def eps_greedy_q_learning_with_table(env, num_episodes=500):
-    q_table = np.zeros((env.num_states_, env.num_actions_))
-    y = 0.95
-    eps = 0.5
-    lr = 0.8
-    decay_factor = 0.999
-    for i in range(num_episodes):
-        s = env.reset()
-        eps *= decay_factor
-        done = False
-        while not done:
-            # select the action with highest cummulative reward
-            if np.random.random() < eps or np.sum(q_table[s, :]) == 0:
-                a = np.random.randint(0, env.num_actions_)
-            else:
-                a = np.argmax(q_table[s, :])
-            # pdb.set_trace()
-            new_s, r, done, _ = env.step(a)
-            q_table[s, a] += r + lr * (
-                    y * np.max(q_table[new_s, :]) - q_table[s, a])
-            s = new_s
-        print('\n--')  #
-    return q_table
-
-
-def create_model(env):
+def create_model(env: MyEnv) -> Sequential:
     model = Sequential()
     model.add(InputLayer(batch_input_shape=(1, env.num_states_)))
     model.add(Dense(36, activation='sigmoid'))
@@ -68,11 +21,19 @@ def create_model(env):
     return model
 
 
-def onehot(num_states, state):
+def onehot(num_states: int, state: int) -> np.ndarray:
     return np.identity(num_states)[state:state + 1]
 
 
-def q_learning(env, num_episodes=1000):
+def predict(model, num_states, state) -> int:
+    return int(np.argmax(model.predict(onehot(num_states, state))))
+
+
+def predict_value(model, num_states, state):
+    return np.max(model.predict(onehot(num_states, state)))
+
+
+def q_learning(env, num_episodes=1000, plot=False):
     # create the keras model
     model = create_model(env)
     # now execute the q learning
@@ -80,7 +41,7 @@ def q_learning(env, num_episodes=1000):
     eps = 0.5
     decay_factor = 0.999
     r_avg_list = []
-    num_states = env.num_states_
+    num_states: int = env.num_states_
     num_actions = env.num_actions_
     for i in range(num_episodes):
         s = env.reset()
@@ -93,45 +54,50 @@ def q_learning(env, num_episodes=1000):
             if np.random.random() < eps:
                 a = np.random.randint(0, num_actions)
             else:
-                a = np.argmax(
-                    model.predict(np.identity(num_states)[s:s + 1]))
+                a = predict(model, num_states, s)
             new_s, r, done, _ = env.step(a)
-            target = r + y * np.max(
-                model.predict(np.identity(num_states)[new_s:new_s + 1]))
-            target_vec = model.predict(np.identity(num_states)[s:s + 1])[0]
+            target = r + y * predict_value(model, num_states, new_s)
+            target_vec = model.predict(onehot(num_states, s))[0]
             target_vec[a] = target
-            model.fit(np.identity(num_states)[s:s + 1],
+            model.fit(onehot(num_states, s),
                       target_vec.reshape(-1, num_actions),
                       epochs=1, verbose=0)
             s = new_s
             r_sum += r
         r_avg_list.append(r_sum / num_episodes)
-    plt.plot(r_avg_list)
-    plt.ylabel('Average reward per game')
-    plt.xlabel('Number of games')
-    plt.show()
+
+    if plot:
+        plt.plot(r_avg_list)
+        plt.ylabel('Average reward per game')
+        plt.xlabel('Number of games')
+        plt.show()
+
     strategy = [
         np.argmax(model.predict(np.identity(num_states)[i:i + 1])[0])
         for i in range(num_states)
     ]
     for i in range(num_states):
-        print("State {:<12s} -> {:<5s} {}".format(
+        print("State {:<12s} -> {:<10s} {}".format(
             state_dict[i], action_dict[strategy[i]],
             model.predict(np.identity(num_states)[i:i + 1])))
     print()
     return strategy
 
 
-env = MyEnv(debug=False)
-strategy = q_learning(env, 1000)
+def main():
+    env = MyEnv(debug=False)
+    strategy = q_learning(env, 1000)
 
-done = False
-state = env.reset(debug=True)
-while not done:
-    a = env.decide(state, strategy)
-    new_state, r, done, _ = env.step(a)
+    done = False
+    state = env.reset(debug=True)
+    while not done:
+        a = env.decide(state, strategy)
+        new_state, r, done, _ = env.step(a)
 
-# table = q_learning_with_table(env)
-# print(table)
-# table_greedy = eps_greedy_q_learning_with_table(env)
-# print(table_greedy)
+    # table = q_learning_with_table(env)
+    # print(table)
+    # table_greedy = eps_greedy_q_learning_with_table(env)
+    # print(table_greedy)
+
+
+main()
