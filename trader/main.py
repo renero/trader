0a89bd0b509e00246b65e1b8b7a43c0143f7331d
Jesus Renero <jesus.renero@gmail.com>
2019-07-16 15:06:26 +0200

    Parameters ready for Environment

--- trader/main.py
+++ trader/main.py
@@ -3,25 +3,21 @@
 # https://adventuresinmachinelearning.com/reinforcement-learning-tutorial-python-keras/
 #
 
-from environment import *
+from environment import Environment
 from qlearning import QLearning
 from trader import Trader
-
-# value_states = ['EVEN', 'WIN', 'LOSE']
-# forecast_states = ['EVEN', 'WIN', 'LOSE']
-# share_states = ['HAVE', 'DONTHAVE']
 
 
 def main():
     trader = Trader()
-    env = Environment(trader)
-    strategy = QLearning(trader).q_learn(env)
+    environment = Environment(trader)
+    strategy = QLearning(trader).q_learn(environment)
 
     done = False
-    state = env.reset(debug=True)
+    state = environment.reset(debug=True)
     while not done:
-        a = env.decide(state, strategy)
-        new_state, r, done, _ = env.step(a)
+        a = environment.decide(state, strategy)
+        state, r, done, _ = environment.step(a)
 
 
 main()
