47740d90c5ee34a10f89ad92889dba918502406c
Jesus Renero <jesus.renero@gmail.com>
2019-09-19 08:14:24 +0200

    scaffolding

--- indicators/indicators.py
+++ indicators/indicators.py
@@ -1,8 +1,40 @@
+import pandas as pd
+import matplotlib as plt
+
 from dictionary import Dictionary
+
+
+def ticks_to_ohlc(input_file):
+    data = pd.read_csv('acciona_2018_modelos_CP_MP_LP_MLP.csv',
+                       sep=',')
+    data['Fecha'] = pd.to_datetime(
+        data['Fecha'], format='%d/%m/%Y %H:%M:%S', errors='coerce')
+    data = data.set_index('Fecha')
+    return data.resample('D').agg('last')
+
+
+def plot_comp(data, column):
+    fig, ax1 = plt.subplots()
+    ax1.plot(data.Price)
+    ax1.grid(b=True, which='major', axis='both',
+             color='lightgrey', linestyle='-', linewidth=1)
+    ax1.grid(b=True, which='minor', color='#999999', linestyle='-',
+             alpha=0.2)
+    ax2 = ax1.twinx()
+    ax2.plot(data[column], 'ro--', linewidth=0.5, markersize=1)
+    ax2.axhline(0, color="red")
+    fig.tight_layout()
+    plt.show()
+
+
+def read_data(file_path):
+    data = pd.read_csv(file_path, sep=';')
+    return data
+
 
 if __name__ == "__main__":
     configuration = Dictionary()
-    input_data = read_data(configuration)
+    input_data = read_data(configuration._input_data)
     konkorde = Konkorde(configuration)
     output = konkorde.compute(input_data)
     save_data(output)
---
+++ indicators/konkorde.py
@@ -0,0 +1,48 @@
+"""
+Compute the actual konkorde indicator.
+Pseudocódigo del Koncorde extraído del Pro RealTime
+BLAI5 KONCORDE v.09
+Versión actualizada y reformulada
+
+    pvi = PositiveVolumeIndex(close)
+    pvim = ExponentialAverage[m](pvi)
+    pvimax = highest[90](pvim)
+    pvimin = lowest[90](pvim)
+    oscp = (pvi - pvim) * 100/ (pvimax - pvimin)
+    nvi =NegativeVolumeIndex(close)
+    nvim = ExponentialAverage[m](nvi)
+    nvimax = highest[90](nvim)
+    nvimin = lowest[90](nvim)
+
+    azul = (nvi - nvim) * 100/ (nvimax - nvimin)
+
+    xmf = MoneyFlowIndex[14]
+    OB1 = (BollingerUp[25](TotalPrice) + BollingerDown[25](TotalPrice)) / 2
+    OB2 = BollingerUp[25](TotalPrice) - BollingerDown[25](TotalPrice)
+    BollOsc = ((TotalPrice - OB1) / OB2 ) * 100
+    xrsi = rsi [14](TotalPrice)
+    STOC = Stochastic[21,3](TotalPrice)
+    marron = (xrsi + xmf + BollOsc + (STOC / 3))/2
+
+    verde = marron + oscp
+    media = ExponentialAverage[m](marron)
+    bandacero= 0
+
+    return
+    verde COLOURED(102,255,102) as "verde",
+    marron COLOURED(255,204,153) as "marron",
+    marron COLOURED(51,0,0) as "lmarron",
+    azul COLOURED(0,255,255) as "azul",
+    verde COLOURED(0,102,0) as "lineav",
+    azul COLOURED(0,0,102) as "lazul",
+    media COLOURED(255,0,0) as "media",
+    bandacero COLOURED(0,0,0) as "cero"
+
+"""
+
+
+class Konkorde(object):
+
+    def __init__(self, configuration):
+        self.configuration = configuration
+
