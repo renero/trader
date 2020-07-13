26970c1764ad6afe0e817ce2427048dcfd9bdb22
Jesus Renero <jesus.renero@gmail.com>
2020-07-13 16:24:39 +0200

    added tests for constructor, fit, correct_encoding and calc_parameters

--- test/predictor/test_cs_encoder.py
+++ test/predictor/test_cs_encoder.py
@@ -27,59 +27,116 @@ class TestCSEncoder(TestCase):
         super(TestCSEncoder, cls).setUpClass()
         # Defining a type A, G, M, Q, W and Z
         data = pd.DataFrame({
-            'o': [50., 70., 30., 45., 7, 100.],
-            'h': [90,  91., 92., 93., 94, 100.],
-            'l': [1., 2., 3., 4., 1., 2.],
-            'c': [51., 75., 15., 85., 62., 0.],
-            'v': [1000, 8300, 2300, 6600, 5000, 1241]
+            'o': [50., 80., 10., 80., 10., 100],
+            'h': [100, 100, 100, 100, 100, 100],
+            'l': [00., 00., 00., 00., 00., 00.],
+            'c': [51., 70., 30., 40., 70., 00.],
+            'v': [100, 830, 230, 660, 500, 120]
         })
-        datec = pd.DataFrame({
+        date_column = pd.DataFrame({
             'Date': pd.date_range('2020-06-01', '2020-06-06', freq='D')
         })
-        data = pd.concat([datec, data], axis=1)
+        data = pd.concat([date_column, data], axis=1)
         data = data.set_index('Date')
+        # mpf.plot(data, columns=['o','h','l','c','v'], type='candle',
+        # style='charles')
         cls.data = data
 
+    def test_CSEncoder(self):
+        """
+        Test the constructor. Normally called without any tick in the
+        arguments, it
+        """
+        cse = CSEncoder(self.params)
+        self.assertEqual(cse.open, 0.)
+        self.assertEqual(cse.close, 0.)
+        self.assertEqual(cse.high, 0.)
+        self.assertEqual(cse.low, 0.)
+        self.assertEqual(cse.min, 0.)
+        self.assertEqual(cse.max, 0.)
+
+        # Initialization
+        self.assertEqual(cse.encoded_delta_close, 'pA')
+        self.assertEqual(cse.encoded_delta_high, 'pA')
+        self.assertEqual(cse.encoded_delta_low, 'pA')
+        self.assertEqual(cse.encoded_delta_max, 'pA')
+        self.assertEqual(cse.encoded_delta_min, 'pA')
+        self.assertEqual(cse.encoded_delta_open, 'pA')
+
+    def test_correct_encoding(self):
+        """
+        Test if this method is correctly capturing that column names reflect
+        what the encoding is saying.
+        """
+        self.assertTrue(
+            CSEncoder(self.params, encoding='ohlc').correct_encoding())
+        self.assertTrue(
+            CSEncoder(self.params, encoding='OHLC').correct_encoding())
+        self.assertFalse(
+            CSEncoder(self.params, encoding='0hlc').correct_encoding())
+        self.assertFalse(
+            CSEncoder(self.params, encoding='ohl').correct_encoding())
+        self.assertFalse(
+            CSEncoder(self.params, encoding='').correct_encoding())
+
     def test_fit(self):
-        encoder = CSEncoder(self.params)
+        """
+        Measure main indicators for encoding of the second tick in the
+        test data. I use the second one to be able to compare it against
+        the first one.
+        """
+        encoder = CSEncoder(self.params, values=self.data.iloc[1])
         encoder = encoder.fit(self.data)
         self.assertEqual(encoder.cse_zero_open, 50.)
-        self.assertEqual(encoder.cse_zero_high, 90.)
-        self.assertEqual(encoder.cse_zero_low, 1.)
+        self.assertEqual(encoder.cse_zero_high, 100.)
+        self.assertEqual(encoder.cse_zero_low, 0.)
         self.assertEqual(encoder.cse_zero_close, 51.)
-        self.assertIs(encoder.fitted, True)
+        self.assertTrue(encoder.fitted)
         for subtype in self.params.subtypes:
             self.assertIsNotNone(encoder.onehot[subtype])
 
-    def test_encode_tick(self):
-        cse = CSEncoder(self.params, self.data.iloc[0])
+    def test_calc_parameters(self):
+        """
+        Test if the parameters computed for a sample tick are correct.
+        """
+        cse = CSEncoder(self.params, self.data.iloc[1])
 
-        self.assertEqual(cse.open, 50., 'Open')
-        self.assertEqual(cse.close, 51., 'Close')
-        self.assertEqual(cse.high, 90., 'High')
-        self.assertEqual(cse.low, 1., 'Low')
-        self.assertEqual(cse.min, 50., 'Min between open and close')
-        self.assertEqual(cse.max, 51., 'Max between open and close')
+        self.assertIsNot(cse.positive, cse.negative)
+        self.assertFalse(cse.positive)
+        self.assertTrue(cse.negative)
 
-        print()
-        print(cse.min_percentile)
-        print(cse.max_percentile)
-        print(cse.mid_body_percentile)
-        print(cse.mid_body_point)
-        print(cse.positive)
-        print(cse.negative)
-        print(cse.has_upper_shadow)
-        print(cse.has_lower_shadow)
-        print(cse.has_both_shadows)
-        print(cse.shadows_symmetric)
-        print(cse.body_in_upper_half)
-        print(cse.body_in_lower_half)
-        print(cse.body_in_center)
-        print(cse.hl_interval_width)
-        print(cse.upper_shadow_len)
-        print(cse.lower_shadow_len)
-        print(cse.upper_shadow_percentile)
-        print(cse.lower_shadow_percentile)
-        print(cse.oc_interval_width)
-        print(cse.body_relative_size)
-        print(cse.shadows_relative_diff)
+        # Percentiles, etc...
+        self.assertLessEqual(cse.body_relative_size, 0.1, 'Body relative size')
+        self.assertEqual(cse.hl_interval_width, 100.)
+        self.assertEqual(cse.oc_interval_width, 10.)
+        self.assertEqual(cse.mid_body_point, 75.)
+        self.assertEqual(cse.mid_body_percentile, 0.75)
+        self.assertEqual(cse.min_percentile, 0.7)
+        self.assertEqual(cse.max_percentile, 0.8)
+        self.assertEqual(cse.upper_shadow_len, 20.)
+        self.assertEqual(cse.upper_shadow_percentile, 0.2)
+        self.assertEqual(cse.lower_shadow_len, 70.)
+        self.assertEqual(cse.lower_shadow_percentile, 0.7)
+        self.assertEqual(cse.body_relative_size, 0.1)
+        self.assertAlmostEqual(cse.shadows_relative_diff, 0.5)
+
+        # Body position.
+        self.assertFalse(cse.body_in_center)
+        self.assertFalse(cse.body_in_lower_half)
+        self.assertTrue(cse.body_in_upper_half)
+        self.assertTrue(cse.has_both_shadows)
+        self.assertTrue(cse.has_lower_shadow)
+        self.assertTrue(cse.has_upper_shadow)
+        self.assertFalse(cse.shadows_symmetric)
+
+    def test_ticks2cse(self):
+        """
+        Test the method in charge of transforming an entire list of
+        ticks into CSE format. It's only goal is to retun an array with
+        all of them.
+        """
+        encoder = CSEncoder(self.params).fit(self.data)
+        cse = encoder.ticks2cse(self.data)
+        self.assertEqual(len(cse), 6)
+        for i in range(len(cse)):
+            self.assertIsInstance(cse[i], CSEncoder)
