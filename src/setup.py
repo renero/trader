71a3ca778169f63104fd2a6ed647783f1915e746
jesus.renero <jesus.renero@gmail.com>
2020-10-06 19:47:10 +0200

    bumped version

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='trader',
-      version='0.2.4',
+      version='0.2.5',
       description='Trader package',
       author='J. Renero, J. Gonzalez',
       packages=['indicators', 'predictor', 'retriever', 'trader', 'updater',
